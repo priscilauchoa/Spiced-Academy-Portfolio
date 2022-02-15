711fcadbf441a6b47742797163c804d841306a95
Priscila Flores <priscilauchoaa@gmail.com>
2022-02-15 17:30:11 +0100

    dom events exercise 3

---
+++ week-2/aliester-encounter/dom-events-homework/exercise3.js
@@ -0,0 +1,27 @@
+var square = document.getElementById("square");
+
+function randomColor() {
+    var arr = [];
+    var a = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var c = Math.floor(Math.random() * 256);
+    arr.push(a, b, c);
+    // console.log("array:", arr);
+    return arr;
+}
+
+var colors;
+
+square.addEventListener("mousedown", function () {
+    colors = randomColor();
+
+    console.log(colors);
+    square.style.backgroundColor =
+        "rgb(" + colors[0] + "," + colors[1] + "," + colors[2] + ")";
+});
+
+square.addEventListener("mouseup", function () {
+    colors = randomColor();
+    square.style.backgroundColor =
+        "rgb(" + colors[0] + "," + colors[1] + "," + colors[2] + ")";
+});
