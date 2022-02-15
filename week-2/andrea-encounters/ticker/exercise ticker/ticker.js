4afe570879bbd7a1aaad43468ae9f282e3590a5f
Priscila Flores <priscilauchoaa@gmail.com>
2022-02-15 15:33:25 +0100

    add cancelAnimationFrame and change style from  selected link

--- week-2/andrea-encounters/ticker/exercise ticker/ticker.js
+++ week-2/andrea-encounters/ticker/exercise ticker/ticker.js
@@ -2,6 +2,7 @@
     var ticker = document.getElementById("ticker-line");
     var left = ticker.offsetLeft;
     var links = document.getElementsByTagName("a");
+    var reqId;
 
     function moveTickerLine() {
         left -= 2;
@@ -15,7 +16,18 @@
             ticker.appendChild(link);
         }
 
-        requestAnimationFrame(moveTickerLine);
+        reqId = requestAnimationFrame(moveTickerLine);
     }
     moveTickerLine();
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("mouseenter", function (e) {
+            cancelAnimationFrame(reqId);
+            e.target.style.backgroundColor = "violet";
+        });
+        links[i].addEventListener("mouseleave", function (e) {
+            e.target.style.backgroundColor = "white";
+            moveTickerLine();
+        });
+    }
 })();
