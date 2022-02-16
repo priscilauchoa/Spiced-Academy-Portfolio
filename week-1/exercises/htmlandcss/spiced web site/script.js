6c4234dfb45e345a062a648a4c5d312341e5d816
Priscila Flores <priscilauchoaa@gmail.com>
2022-02-16 20:10:14 +0100

    menu hamburguer transition

--- week-1/exercises/htmlandcss/spiced web site/script.js
+++ week-1/exercises/htmlandcss/spiced web site/script.js
@@ -1,18 +1,20 @@
 (function () {
     var menuButton = document.getElementById("menu-button"); //button
-    var sideNav = document.getElementById("side-nav"); //<p>
+    var sideNav = document.getElementsByClassName("side-nav"); //<p>
     var menuX = document.getElementById("menu-x"); //<p>
+    var overlay = document.getElementsByClassName("overlay"); //<p>
+    // console.log(sideNav);
 
     menuButton.addEventListener("click", function () {
-        console.log("menuButton");
-        // sideNav.style.visibility = "visible";
-        sideNav.id = "on";
+        // console.log("menuButton");
+        sideNav[0].classList.add("on");
+        overlay[0].classList.add("showOverlay");
         // console.log("added class on");
     });
     menuX.addEventListener("click", function () {
-        console.log("on");
-        sideNav.id = "side-nav";
-        // sideNav.style.visibility = "hidden";
-        // console.log("click  on X button");
+        // console.log("on");
+        sideNav[0].classList.remove("on");
+        overlay[0].classList.remove("showOverlay");
+        // console.log("click  on  button x");
     });
 })();
