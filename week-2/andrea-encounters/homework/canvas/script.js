41b6ce20f1e8f943111e30e1bba1e9d05d6ef613
Priscila Flores <priscilauchoaa@gmail.com>
2022-02-16 22:26:01 +0100

    moving sticker - bonus

--- week-2/andrea-encounters/homework/canvas/script.js
+++ week-2/andrea-encounters/homework/canvas/script.js
@@ -1,23 +1,19 @@
 (function () {
-    var bigCanvas = document.getElementById("big-canvas");
     var canvas = document.getElementById("canvas");
+    // var bigCanvas = document.getElementById("big-canvas");
     // console.log("canvas", canvas);
 
-    var bigC = bigCanvas.getContext("2d");
     var c = canvas.getContext("2d");
+    // var bigC = bigCanvas.getContext("2d");
 
-    // BIG Canvas
-
-    bigC.beginPath();
-    bigC.fillStyle = "violet";
-    bigC.fillRect(0, 0, bigCanvas.width, bigCanvas.height);
-    bigC.strokeStyle = "black;";
-    bigC.lineWidth = 4;
-    bigC.beginPath();
+    // // BIG Canvas
+    // bigC.beginPath();
+    // // bigC.fillStyle = "red";
+    // bigC.fillRect(0, 0, bigCanvas.width, bigCanvas.height);
 
     // Canvas inside
     c.beginPath();
-    c.fillStyle = "pink";
+    c.fillStyle = "transparent";
     c.fillRect(0, 0, canvas.width, canvas.height);
     c.strokeStyle = "black;";
     c.lineWidth = 4;
@@ -100,4 +96,26 @@
     c.stroke();
 
     // function stickerMoves() {}
+
+    var positionX = 0;
+    var positionY = 0;
+    var move = 15;
+
+    function stickerMoves(e) {
+        if (e.keyCode === 38) {
+            positionY -= move;
+            canvas.style.top = positionY + "px";
+        } else if (e.keyCode === 40) {
+            positionY += move;
+            canvas.style.top = positionY + "px";
+        } else if (e.keyCode === 37) {
+            positionX -= move;
+            canvas.style.left = positionX + "px";
+        } else if (e.keyCode === 39) {
+            positionX += move;
+            canvas.style.left = positionX + "px";
+        }
+    }
+    // console.log(stickerMoves());
+    document.addEventListener("keydown", stickerMoves);
 })();
