4535afcc7174aba9b1ca6eccd3e002c2075121a0
Priscila Flores <priscilauchoaa@gmail.com>
2022-02-21 14:27:17 +0100

    change  background color

--- week-2/david-encounters/jquery/ticker-jquery/ticker.js
+++ week-2/david-encounters/jquery/ticker-jquery/ticker.js
@@ -1,45 +1,49 @@
 (function () {
-    var ticker = $("#ticker-line");
+    var headlines = $("#ticker-line");
     // var ticker = document.getElementById("ticker-line");
-    var left = ticker.offset().left;
-    // var left = ticker.offsetLeft;
+    var left = headlines.offset().left;
+    // var left = headlines.offsetLeft;
 
     var links = $("a");
     // var links = document.getElementsByTagName("a");
     var reqId;
-
     function moveTickerLine() {
-        left -= 2;
-        ticker.css(left, left + "px");
+        left -= 5;
+        headlines.css("left", left + "px");
+        var link = links.eq(0);
+        console.log(link);
 
-        var link = links[0];
+        if (left <= -link.outerWidth()) {
+            left += link.outerWidth();
+            // console.log(link.outerWidth);
+            headlines.css("left", left + "px");
+            headlines.append(link);
 
-        if (left <= -link.offsetWidth) {
-            left += link.offsetWidth;
-            ticker.style.left = left + "px";
-            ticker.appendTo(link);
-            // ticker.appendChild(link);
+            links = $("a");
+            // headlines.appendChild(link);
         }
+
+        //3 divs  pane 1 (pane 1 tem zindex100 )pane2 slide // 2 main container cm as imagens  left hight zindex - prevent default function pra colocar no mouse down e mousemove -- usar  offsetx offsety diz onde o mouse esta -
+        //the current slide deve verificar se é o mesmo do bar
+        //
 
         reqId = requestAnimationFrame(moveTickerLine);
     }
     moveTickerLine();
 
-    for (var i = 0; i < links.length; i++) {
-        links[i].on("mouseenter", function (e) {
-            cancelAnimationFrame(reqId);
-            e.target.css("backgroundColor", "violet");
-            // links[i].addEventListener("mouseenter", function (e) {
-            //     cancelAnimationFrame(reqId);
-            //     e.target.style.backgroundColor = "violet";
-        });
-        links[i].on("mouseleave", function (e) {
-            e.target.css("backgroundColor", "white");
-            moveTickerLine();
-        });
-        // links[i].addEventListener("mouseleave", function (e) {
-        //     e.target.style.backgroundColor = "white";
-        //     moveTickerLine();
-        // });
-    }
+    links.on("mouseenter", function (e) {
+        cancelAnimationFrame(reqId);
+        $(e.target).css("backgroundColor", "violet");
+        // links[i].addEventListener("mouseenter", function (e) {
+        //     cancelAnimationFrame(reqId);
+        //     e.target.style.backgroundColor = "violet";
+    });
+    links.on("mouseleave", function (e) {
+        $(e.target).css("backgroundColor", "white");
+        moveTickerLine();
+    });
+    // links[i].addEventListener("mouseleave", function (e) {
+    //     e.target.style.backgroundColor = "white";
+    //     moveTickerLine();
+    // });
 })();
