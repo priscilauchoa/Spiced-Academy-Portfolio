efabb07e636fca0675bbd7196c2a35dce4ceb55c
Priscila Flores <priscilauchoaa@gmail.com>
2022-03-17 22:32:59 +0100

    Tests jest spotify exercise

--- week-6/testing/exercises/2/albums.js
+++ week-6/testing/exercises/2/albums.js
@@ -1,14 +1,29 @@
-const spotify = require('./spotify');
+const spotify = require("./spotify");
 
-exports.getAlbumNames = function(q) {
-    return spotify.search(q, 'album').then(function(data) {
+exports.getAlbumNames = function (q) {
+    console.log("------->>", spotify);
+    return spotify.search(q, "album").then(function (data) {
         const albumNames = [];
         for (var i = 0; i < data.albums.items.length; i++) {
-            albumNames.push(
-                data.albums.items[i].name
-            );
+            albumNames.push(data.albums.items[i].name);
         }
         albumNames.sort();
         return albumNames;
     });
+};
+let name = "Fear of the dark";
+let data = {
+    albums: {
+        items: [
+            {
+                name: "Hi",
+            },
+            {
+                name: name,
+            },
+            {
+                name,
+            }
+        ],
+    },
 };
--- week-6/testing/exercises/2/albums.test.js
+++ week-6/testing/exercises/2/albums.test.js
@@ -1,7 +1,23 @@
 const { getAlbumNames } = require("./albums");
-const spotify = require("./spotify");
+// const spotify = require("./spotify");
 
-jest.mock("./spotify");
+jest.mock("./spotify", () => {
+    return {
+        search: () => {
+            return new Promise((resolve) => {
+                resolve({
+                    albums: {
+                        items: [
+                            {
+                                name: "Fear Inoculum",
+                            },
+                        ],
+                    },
+                });
+            });
+        },
+    };
+});
 
 test("album names are in alphabetical order", () => {
     return getAlbumNames("meat loaf").then((albumNames) => {
