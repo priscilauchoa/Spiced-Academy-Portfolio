7db5778bb7f1e0d01d6ae046663519dff831973e
Priscila Flores <priscilauchoaa@gmail.com>
2022-03-17 17:26:19 +0100

    jest exercise 3

--- week-6/testing/exercises/3/fn.js
+++ week-6/testing/exercises/3/fn.js
@@ -1,2 +1,22 @@
-module.exports = function fn() {
+module.exports = function fn(strings) {
+    // If a string as first argument
+    if (typeof strings == "string") {
+        return strings.split("").reverse().join("");
+    }
+    // If a array as first argument
+    else if (Array.isArray(strings)) {
+        const newStrings = [];
+        for (let i = 0; i < strings.length; i++) {
+            if (typeof strings[i] == "string") {
+                newStrings.push(strings[i].split("").reverse().join(""));
+            } else {
+                newStrings.push(null);
+            }
+        }
+        return newStrings;
+    }
+    // neither a string nor an array
+    else {
+        return null;
+    }
 };
