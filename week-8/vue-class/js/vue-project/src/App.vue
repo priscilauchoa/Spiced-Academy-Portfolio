e0483b2f0a0d1853c00f8f5329fa467cfe486992
Priscila-Flores <priscilauchoaa@gmail.com>
2023-02-14 20:04:32 +0100

    import menu component

--- week-8/vue-class/js/vue-project/src/App.vue
+++ week-8/vue-class/js/vue-project/src/App.vue
@@ -1,25 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import Menu from '@/components/Menu.vue'
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
+    <Menu />
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+     
     </div>
   </header>
 
