43f5199965994c0a2c5134a056fa13364ab1590a
Priscila-Flores <priscilauchoaa@gmail.com>
2023-02-17 13:27:55 +0100

    create login form

---
+++ week-8/vue-class/js/vue-project/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
+function login() {
+        console.log(email.value, password.value)
+    }
+</script>
+
+<template>
+
+    {{ email }}
+    <form action="" v-on:submit.prevent="login">
+    <input type="text" placeholder="email" v-model="email">
+    
+
+    <input type="text" placeholder="password" v-model="password">
+    <button type="submit">Submit</button>
+    </form>
+</template>
+
+<style></style>
