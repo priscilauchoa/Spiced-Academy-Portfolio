db40c268b8568993e153c24c6d9ffd166b136712
Priscila-Flores <priscilauchoaa@gmail.com>
2023-02-17 17:09:04 +0100

    rename app component

--- week-8/vue-class/js/rocketseat-react/reactapp-first-class/src/pages/Home/index.tsx
+++ week-8/vue-class/js/rocketseat-react/reactapp-first-class/src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
+import './style.css'
 
-function Home() {
+export function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='container'>
       <h1>Attendence List</h1>
       <input type="text" placeholder='Name'></input>
       <button>Submit</button>
-    </>
+    </div>
   )
 }
 
-export default Home
